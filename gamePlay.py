da4bc7ae670db07f0de1dfbf7a29e0f723fdd262
Leoul Tilahun <33269725+PrinceUmbrella@users.noreply.github.com>
2020-10-09 16:27:37 -0400

    removed the ui part

--- gamePlay.py
+++ gamePlay.py
@@ -1,8 +1,6 @@
 from gameClasses.Question import Question
-from UI.Display import Game
 from types import SimpleNamespace
 import json
-data = '{"name": "John Smith", "hometown": {"name": "New York", "id": 123}}'
 
 # Parse JSON into an object with attributes corresponding to dict keys.
 questionInput = json.load(open("questionsInput.json"),
@@ -12,17 +10,3 @@ questionInput = json.load(open("questionsInput.json"),
 questionObj = Question(
     questionInput.questionsList[0].questions[0], questionInput.questionsList[0].category)
 print(questionObj)
-
-# Display Question
-result = Game(questionObj.getQuestion(), questionObj.getOptions())
-
-print("User's response was: {}".format(repr(result)))
-
-questionObj = Question(
-    questionInput.questionsList[0].questions[1], questionInput.questionsList[0].category)
-print(questionObj)
-
-# Display Question
-result = Game(questionObj.getQuestion(), questionObj.getOptions())
-
-print("User's response was: {}".format(repr(result)))
