ce9eba055b3b6f0096e6e992f2111e6c8883e003
Jussepen <33269715+Jussepen@users.noreply.github.com>
2020-10-10 20:26:30 -0400

    Comments to the question object

--- gameClasses/Question.py
+++ gameClasses/Question.py
@@ -1,6 +1,11 @@
 from prettytable import PrettyTable
 import random
 
+"""
+Question object has the question and array of possible answers.
+Holds the category, answer, and worth of each question
+"""
+
 
 class Question:
     def __init__(self, questionJson, category):
@@ -11,40 +16,53 @@ class Question:
         self.setPoints()
         self.setOptions()
         self.seen = False
+    # function that lets us print the question
 
     def __str__(self):
         return self.question
+    # function that sets option
 
     def setOptions(self):
         random.shuffle(self.questionJson.options)
         self.options = self.questionJson.options
+    # function that allows us to get the possible answers
 
     def getOptions(self):
         return self.options
+    # function that sets the question
 
     def setQuestion(self):
         self.question = self.questionJson.question
+    # function that lets us get the question
 
     def getQuestion(self):
         return self.question
+    # function that set category
 
     def getCategory(self):
         return self.category
+    # function that sets the answer
 
     def setAnswer(self):
         self.answer = self.questionJson.answer
+    # function that gets the answer
 
     def getAnswer(self):
         return self.answer
+    # function that sets points
 
     def setPoints(self):
         self.points = self.questionJson.worth
+    # function that gets points
 
     def getPoints(self):
         return self.points
+    # function that lets us know if the card has been seen before
 
     def isSelected(self):
         return self.seen
+    # using the PrettyTable import nicely displays the question object
+    # With a question title and the options
 
     def printQuestion(self):
         questionCard = PrettyTable([self.question])
